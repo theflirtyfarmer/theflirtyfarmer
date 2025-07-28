2e3b2a90955f9dc6cb0e715420c31aeb6f03118e
theflirtyfarmer <sara@theflirtyfarmer.com>
2025-07-28 14:38:37 -0400

    Update vite base to / for root GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
